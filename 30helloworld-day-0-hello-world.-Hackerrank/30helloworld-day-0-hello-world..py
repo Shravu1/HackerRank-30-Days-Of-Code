3a1faa33a38533239279a99e25004f9baa4facfc
Shravu1 <shravanir112@gmail.com>
2026-07-18 17:12:07 +0530

    Force committed solution - Time: O(N), Space: O(N)

---
+++ 30helloworld-day-0-hello-world.-Hackerrank/30helloworld-day-0-hello-world..py
@@ -0,0 +1,12 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+print(input_string)
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
