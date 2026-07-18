23e3ca35767b8d50be7ff36efaa274f6718fc2e4
Shravu1 <shravanir112@gmail.com>
2026-07-18 19:57:26 +0530

    Force committed solution

---
+++ 18929-day-5-loops-Hackerrank/18929-day-5-loops.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    for i in range(1,11):
+        print(f"{n} x {i} = {n * i}")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
