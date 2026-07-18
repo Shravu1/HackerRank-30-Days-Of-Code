d5e830033f755effead4ef1df8daf27b75b23582
Shravu1 <shravanir112@gmail.com>
2026-07-18 17:17:24 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 18663-day-1-data-types-Hackerrank/18663-day-1-data-types.py
@@ -0,0 +1,19 @@
+
+# Declare second integer, double, and String variables.
+j=int(input())
+e=float(input())
+t=str(input())
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i+j)
+# Print the sum of the double variables on a new line.
+print(d+e)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+t)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
