f37f1239982b9f4c3516bb3625e526ba6eee4a30
Shravu1 <shravanir112@gmail.com>
2026-07-20 20:16:54 +0530

    Force committed solution - Time: O(N), Space: O(N)

---
+++ 18927-day-6-lets-review-Hackerrank/18927-day-6-lets-review.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT\
+for _ in range(int(input())):
+    s=input()
+    even=s[::2]
+    odd=s[1::2]
+    print(even,odd)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
